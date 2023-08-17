33d6a427c0d30cc5d5b851e0113d4bdec1d0e8b0
torikulswe <torikulswe@gmail.com>
2023-08-17 23:03:26 +0600

    geometry area calculation project

---
+++ scripts/area-calculator.js
@@ -0,0 +1,36 @@
+function calculateTriangleArea(params) {
+    // get triangle base value
+    const baseField = document.getElementById('triangle-base');
+    const baseValueText = baseField.value;
+    const base = parseFloat(baseValueText);
+    
+    // get triangle height value
+    const heightField = document.getElementById('triangle-height');
+    const heightValueText = heightField.value;
+    const height = parseFloat(heightValueText);
+
+    const area = 0.5 * base * height;
+    
+    // show triangle area
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
+}
+
+function calculateRectangleArea(params) {
+    // get rectangle width
+    const widthField = document.getElementById('rectangle-width');
+    const widthValueText = widthField.value;
+    const width = parseFloat(widthValueText);
+    
+    // get rectangle length
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText);
+
+    // calculate area
+    const area = width * length;
+
+    // show rectangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+}
