03776e468fc8b6d31cbe5038a3927fdcabf725f6
torikulswe <torikulswe@gmail.com>
2023-08-18 12:11:48 +0600

    added reusable function for all

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -1,42 +1,20 @@
 function calculateTriangleArea(params) {
-    // get triangle base value
-    const baseField = document.getElementById('triangle-base');
-    const baseValueText = baseField.value;
-    const base = parseFloat(baseValueText);
-    
-    // get triangle height value
-    const heightField = document.getElementById('triangle-height');
-    const heightValueText = heightField.value;
-    const height = parseFloat(heightValueText);
-
-    const area = 0.5 * base * height;
-    
-    // show triangle area
-    const areaSpan = document.getElementById('triangle-area');
-    areaSpan.innerText = area;
+    ;
+    const base = getInputValue('triangle-base');
+    const height = getInputValue('triangle-height')
+    const area = (0.5 * base * height);
+    setElementInnerText('triangle-area', area);
 }
 
+// rectangle
 function calculateRectangleArea(params) {
-    // get rectangle width
-    const widthField = document.getElementById('rectangle-width');
-    const widthValueText = widthField.value;
-    const width = parseFloat(widthValueText);
-    
-    // get rectangle length
-    const lengthField = document.getElementById('rectangle-length');
-    const lengthValueText = lengthField.value;
-    const length = parseFloat(lengthValueText);
-
-    // calculate area
+    const width = getInputValue('rectangle-width');
+    const length = getInputValue('rectangle-length')
     const area = width * length;
-
-    // show rectangle area
-    const rectangleAreaSpan = document.getElementById('rectangle-area');
-    rectangleAreaSpan.innerText = area;
+    setElementInnerText('rectangle-area', area);
 }
 
-
-// reusable function 
+// parallelogram
 function calculateParallelogramArea() {
     const base = getInputValue('parallelogram-base');
     const height = getInputValue('parallelogram-height')
@@ -49,13 +27,13 @@ function calculateRhombusArea() {
     const diagonal2 = getInputValue('rhombus-d2');
     const area = (0.5 * diagonal1 * diagonal2);
     setElementInnerText('rhombus-area', area)
-
 }
+
 // pentagon
 function calculatePentagonArea() {
     const base = getInputValue('base');
     const height = getInputValue('height')
-    const area = (0.5 * base * height );
+    const area = (0.5 * base * height);
     setElementInnerText('pentagon-area', area);
 }
 // ellipse
@@ -65,7 +43,6 @@ function calculateEllipseArea() {
     const area = (3.1416 * major * minor).toFixed(2);
     setElementInnerText('ellipse-area', area)
 }
-
 
 // reusable get input value field in number
 function getInputValue(fieldId) {
