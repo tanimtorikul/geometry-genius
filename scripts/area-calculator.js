d516e301610af5ee0747e8bdc01b473f947369b8
torikulswe <torikulswe@gmail.com>
2023-08-18 12:05:56 +0600

    parallelogram, rhombus, pentagon, ellipse added

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -34,3 +34,49 @@ function calculateRectangleArea(params) {
     const rectangleAreaSpan = document.getElementById('rectangle-area');
     rectangleAreaSpan.innerText = area;
 }
+
+
+// reusable function 
+function calculateParallelogramArea() {
+    const base = getInputValue('parallelogram-base');
+    const height = getInputValue('parallelogram-height')
+    const area = base * height;
+    setElementInnerText('parallelogram-area', area);
+}
+// rhombus
+function calculateRhombusArea() {
+    const diagonal1 = getInputValue('rhombus-d1');
+    const diagonal2 = getInputValue('rhombus-d2');
+    const area = (0.5 * diagonal1 * diagonal2);
+    setElementInnerText('rhombus-area', area)
+
+}
+// pentagon
+function calculatePentagonArea() {
+    const base = getInputValue('base');
+    const height = getInputValue('height')
+    const area = (0.5 * base * height );
+    setElementInnerText('pentagon-area', area);
+}
+// ellipse
+function calculateEllipseArea() {
+    const major = getInputValue('ellipse-major-radius');
+    const minor = getInputValue('ellipse-minor-radius');
+    const area = (3.1416 * major * minor).toFixed(2);
+    setElementInnerText('ellipse-area', area)
+}
+
+
+// reusable get input value field in number
+function getInputValue(fieldId) {
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+// reusable set text 
+function setElementInnerText(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
+
